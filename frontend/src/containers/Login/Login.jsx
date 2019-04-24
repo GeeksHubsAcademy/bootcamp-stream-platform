095a0a2595966e65113b601fb5bf4a2ed87348c6
jupegarnica <jupegarnica@gmail.com>
2019-04-24 17:47:21 +0200

    redux explanation

--- frontend/src/containers/Login/Login.jsx
+++ frontend/src/containers/Login/Login.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from '@reach/router';
+// import { loggedIn } from '../../redux/actions'
+
+
 
 class Login extends Component {
   login = () => {
-    this.props.dispatch({
-      type: 'LOGGED_IN',
-      user: { name: 'juan' },
-    });
+    // loggedIn(pass, email)
+    // .then(
+    //   () => this.navigation.push('/home')
+    // )
+    // .catch(
+    //   error => this.setState({error})
+    // )
   };
   render() {
     if (this.props.isLogged) {
@@ -17,6 +23,7 @@ class Login extends Component {
          <section className='LoginView'>
            <h1>Login</h1>
            <button onClick={this.login}>login</button>
+
          </section>
        );
   }
