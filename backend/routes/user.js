d137baa4ba593677505569c1e5057cd6fadbd29c
DavidPinilla1 <metroid_300@hotmail.com>
2019-04-25 17:31:25 +0200

    added new Bootcamp post endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,22 @@
 const router = require('express').Router();
 const config = require('../config/password')
 const User = require('../models/User');
-const Bootcamp=require('../models/Bootcamp')
+const Bootcamp = require('../models/Bootcamp')
+const bcrypt = require('bcrypt');
 router.get('/all', (req, res) => {
     User.find({}).then(users => res.send(users)).catch(err => res.status(500).send(err))
 })
 router.get('/:user_id', (req, res) => {
     User.findById(req.params.user_id).then(userFound => {
-        Bootcamp.find({user:{_id:userFound._id}}).then(Bootcamps=>{
-            res.status(200).json({userFound,Bootcamps})
+        Bootcamp.find({
+            user: {
+                _id: userFound._id
+            }
+        }).then(Bootcamps => {
+            res.status(200).json({
+                userFound,
+                Bootcamps
+            })
         })
     }).catch(err => res.status(500).send(err))
 })
@@ -17,25 +25,28 @@ router.post('/login', (req, res) => {
         email: req.body.email
     }).then(userFound => {
         if (!userFound) {
-            return res.send({message:'Email or password wrong'});
+            return res.send({
+                message: 'Email or password wrong'
+            });
         }
         // if(!userFound.confirmed) {       /* This will be added when the confirmed email property will be created at the User schema */
         //     return res.send('error','Email or password wrong'); 
         // }
         bcrypt.compare(req.body.password, userFound.password).then(isMatch => {
-            if (!isMatch){
-                return res.status(400).send({message:'Email or password wrong'});
+            if (!isMatch) {
+                return res.status(400).send({
+                    message: 'Email or password wrong'
+                });
             }
-            userFound.generateAuthToken().then(token=>{
-                res.header('Authorization',token).send(userFound)
+            userFound.generateAuthToken().then(token => {
+                res.header('Authorization', token).send(userFound)
             }).catch(console.log)
         }).catch(console.log)
     })
 });
 
 router.post('/register', (req, res) => {
-    console.log(req.body)
-    // jwt.sign({
+    // jwt.sign({                     /* This will be added when the confirmed email property will be created at the User schema */
     //     user: req.body.email
     // }, config.EMAIL_SECRET, {
     //     expiresIn: '2d'
@@ -49,15 +60,23 @@ router.post('/register', (req, res) => {
     //             <a href="${url}">Click here to confirm your email</a><br>
     //                 The link above will expire in 48 hours.</h3> `
     //     }).then(()=>{
-            new User(
-                {...req.body}
-            ).save().then(user =>{
-                console.log('then')
-                res.send({user,'info': 'user succesfully created, please confirm your email '})
-            }).catch(err =>{
-                res.send({err,'error':'Email already in use, please choose another email'})
-            })
-        // }).catch(console.log)
+    new User({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: req.body.password
+    }).save().then(user => {
+        res.send({
+            user,
+            'info': 'user succesfully created, please confirm your email '
+        })
+    }).catch(err => {
+        res.send({
+            err,
+            'error': 'Email already in use, please choose another email'
+        })
+    })
+    // }).catch(console.log)
     // })
 });
 module.exports = router;
