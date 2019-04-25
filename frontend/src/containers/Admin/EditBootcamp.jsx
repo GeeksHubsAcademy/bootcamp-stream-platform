4d408527f8c156de3cd73b9eca39e0dd486cecaa
Luis Rodriguez <luisrdrgz.ing@gmail.com>
2019-04-25 18:49:50 +0200

    save or create bootcamp done

--- frontend/src/containers/Admin/EditBootcamp.jsx
+++ frontend/src/containers/Admin/EditBootcamp.jsx
@@ -2,36 +2,48 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './EditBootcamp.scss';
 
-class EditBootcamp extends Component{
-    state ={
-            bootcampDetails: this.props.bootcamp || []
-    }
+class EditBootcamp extends Component {
+    state = this.props.bootcamp ||   {
+            title: '',
+            description: '',
+            startsAt: '',
+        
+        }
+
     handleChange = (ev) => {
-        this.setState.bootcampDetails({ bootcampDetails: ev.target.value });
+        this.setState({  [ev.target.name] : ev.target.value });
     }
-    render(){
+    saveBootcamp = () => {
+        // this.props.dispatch({type:'SAVE_BOOTCAMP',payload: this.state.id})
+        console.log('Saved', this.state)
+    }
+    render() {
         console.log(this.props.bootcamp)
-        return(
-            
+        return (
+
             <div className="editBootcamp">
                 <div className="content-edit">
-                    <input onChange={this.handleChange} value={this.state.bootcampDetails.title || ''} type="text"/>
-                    <input onChange={this.handleChange} value={this.state.bootcampDetails.description || ''} type="text"/>
-                    <input onChange={this.handleChange} value={this.state.bootcampDetails.startsAt || ''} type="text"/>
-                    <input onChange={this.handleChange} value={this.state.bootcampDetails.title || ''} type="text"/>
+                    <input onChange={this.handleChange} value={this.state.title || ''} name="title" type="text" />
+                    <input onChange={this.handleChange} value={this.state.description || ''} name="description" type="text" />
+                    <input onChange={this.handleChange} value={this.state.startsAt || ''} name="startsAt" type="date" />
+                    <input onChange={this.handleChange} value={this.state.weeksDuration || ''} name="weeksDuration" type="text" />
                 </div>
                 <div>
-                    <button>Save</button>
+                    <button onClick={() => this.saveBootcamp(this.state)}>
+                        {(this.props.bootcamp ? 'Save' : 'Create')}</button>
                     <button>Delete</button>
+                </div>
+                <div>
+                    
                 </div>
             </div>
         )
     }
 }
-const mapStateToProps = ( {bootcamps}, {id}) => ({ bootcamp: bootcamps.find(el => String(el._id) === id) });
+const mapStateToProps = ({ bootcamps }, { id }) => ({ bootcamp: bootcamps.find(el => String(el._id) === id) });
 const mapDispatchToProps = dispatch => ({ dispatch });
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
+    mapStateToProps,
+    mapDispatchToProps,
 )(EditBootcamp);
