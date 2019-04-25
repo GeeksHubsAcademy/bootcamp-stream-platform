bdca84d1a854a4a2968cb4d8a13dbfecc8fb6d45
Luis Rodriguez <luisrdrgz.ing@gmail.com>
2019-04-25 17:31:29 +0200

    props bootcamp id comunicated

--- frontend/src/containers/Admin/EditBootcamp.jsx
+++ frontend/src/containers/Admin/EditBootcamp.jsx
@@ -3,20 +3,22 @@ import { connect } from 'react-redux';
 import './EditBootcamp.scss';
 
 class EditBootcamp extends Component{
-
-
+    state ={
+            bootcampDetails: this.props.bootcamp || []
+    }
+    handleChange = (ev) => {
+        this.setState.bootcampDetails({ bootcampDetails: ev.target.value });
+    }
     render(){
-        
-        console.log(this.props.bootcamps)
-      //  const bootcamp = this.props.bootcamps_id;
-        const bootc_id = 123
+        console.log(this.props.bootcamp)
         return(
+            
             <div className="editBootcamp">
                 <div className="content-edit">
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
-                    <input type="text"/>
+                    <input onChange={this.handleChange} value={this.state.bootcampDetails.title || ''} type="text"/>
+                    <input onChange={this.handleChange} value={this.state.bootcampDetails.description || ''} type="text"/>
+                    <input onChange={this.handleChange} value={this.state.bootcampDetails.startsAt || ''} type="text"/>
+                    <input onChange={this.handleChange} value={this.state.bootcampDetails.title || ''} type="text"/>
                 </div>
                 <div>
                     <button>Save</button>
@@ -26,7 +28,7 @@ class EditBootcamp extends Component{
         )
     }
 }
-const mapStateToProps = ( {bootcamps}) => ({ bootcamps });
+const mapStateToProps = ( {bootcamps}, {id}) => ({ bootcamp: bootcamps.find(el => String(el._id) === id) });
 const mapDispatchToProps = dispatch => ({ dispatch });
 
 export default connect(
