e57f50d7bc16c13977db89490d1ea5ba677ccd55
datata <danitt15@gmail.com>
2019-04-25 16:26:32 +0200

    MyBootcamps list done

--- frontend/src/containers/Profile/MyBootcamps.jsx
+++ frontend/src/containers/Profile/MyBootcamps.jsx
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 
+//importamos redux
+import { connect } from 'react-redux';
 
 class MyBootcamps extends Component {
 
   render() {
+    //console.log(JSON.stringify(this.props.bootcamps));
+    console.log(this.props.bootcamps);
     return (
         <section className="MyBootcampsView">
-          <h1>MyBootcamps</h1>
+          <h1>
+            
+          {this.props.bootcamps.map(bootcamp => <div key={bootcamp._id}>{bootcamp.title}</div>)}
+            
+          </h1>
+
+
+          {/* <Link className="MyBootcamps" to={'/bootcamp/' + props.bootcamp.user.id} >
+            <h1></h1>
+            <p></p>
+          </Link> */}
+
+
+
         </section>
     )
   }
 }
 
-export default MyBootcamps;
+//export default MyBootcamps;
+
+
+//export default MyBootcamps;
+const mapStateToProps = ({bootcamps}) => ({ bootcamps });
+const mapDispatchToProps = dispatch => ({ dispatch });
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(MyBootcamps);
