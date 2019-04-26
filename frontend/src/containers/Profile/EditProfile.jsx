65d5187b7f075a04b674810ffc3140f9df88f40c
Pilar Argudo <pargudo@disid.com>
2019-04-26 17:01:34 +0200

    Replace TextField for FormControl for name input

--- frontend/src/containers/Profile/EditProfile.jsx
+++ frontend/src/containers/Profile/EditProfile.jsx
@@ -8,28 +8,71 @@ import './EditProfile.scss';
 // form styles
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+// form error styles
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 
 
 class EditProfile extends Component {
 
   // state initial
-  state =  this.props.user || {}
-
-  saveProfile = (e) => {
-    e.preventDefault();
-    console.log('Saved', this.state)
-  };
+  //state =  this.props.user || {}
+  state = {
+    name: this.props.user.name,
+    surname: this.props.user.surname,
+    email: this.props.user.email,
+    password: this.props.user.password,
+    password2: this.props.user.password2,
+    // TODO el input type submit tiene un name?
+    errorName: undefined,
+    errorSurname: undefined,
+    errorEmail: undefined,
+    errorPassword: undefined,
+    errorPassword2: undefined
+  }
 
   // TODO
   deleteProfile = (e) => {
     e.preventDefault();
-    console.log('Delete', this.state.bootcamp)
+    //console.log('Delete', this.state.bootcamp)
   };
+
+  // TODO post action
+  saveProfile = (e) => {
+    e.preventDefault();
+    //console.log('Saved', this.state)
+    this.validate();
+  };  
 
   handleChange = name => event => {
-    this.setState({ [name]: event.target.value });
+    this.setState(
+      {[name]: event.target.value.trim() },
+      () =>{
+        console.log( this.state )
+        // validate onChange inside callback
+        this.validate();
+      }
+      );
   };
 
+  validate = () => {
+    console.log('hola, estamos validando!');
+
+    if( this.state.name === ''){
+      this.setState({ errorName: 'Please, write your name'});
+      console.log('campo vacío');
+    }
+    // if( this.state.password !== this.state.password2){
+    //   this.setState({ errorPassword2: 'las contraseñas no coinciden'});
+    //   console.log('no coinciden');
+    // } else {
+    //   this.setState({ errorPassword2: undefined});
+    //   console.log(' coinciden');      
+    // }
+
+  }
   render() {
     //console.log(this.props);
 
@@ -42,7 +85,7 @@ class EditProfile extends Component {
     
       <section className="EditProfileView">
 
-          <h1>Hi {this.state.name }</h1>
+          <h1>Hi {this.state.name}</h1>
           <p>Edit your profile: </p>
           <form autoComplete="off"
                 onClick={this.saveProfile}>
@@ -50,7 +93,7 @@ class EditProfile extends Component {
             {/* TODO
               error attribute }             
             */}
-            <TextField
+            {/* <TextField
               id="standard-name"
               label="Name"
               name="name"    
@@ -59,7 +102,27 @@ class EditProfile extends Component {
               onChange={this.handleChange("name")}
               margin="normal"
               required
-            />
+            /> */}
+ 
+       <FormControl className="formControl" error={!!this.state.errorName }>
+              <InputLabel htmlFor="component-name">Name</InputLabel>
+              <Input
+                id="component-name"
+                value={this.state.name}
+                onChange={this.handleChange("name")}
+                aria-describedby="component-name-text"
+                required
+              />              
+              {/* <FormHelperText 
+                id="component-error-text"
+                className={ this.state.errorName ? 'visible': 'hidden'}>
+                {this.state.errorName}
+              </FormHelperText> */}
+              {this.state.errorName && 
+              <FormHelperText id="component-error-text">{this.state.errorName}</FormHelperText>
+              }  
+
+            </FormControl>
 
             <TextField
               id="standard-surname"
@@ -88,7 +151,8 @@ class EditProfile extends Component {
               id="standard-password-input"
               label="Change password"
               name="password"
-              type="password"   
+              type="password"  
+              autoComplete="current-password" 
               className="textField"
               margin="normal"
             />
@@ -96,9 +160,8 @@ class EditProfile extends Component {
             <TextField
               id="standard-repeat-password-input"
               label="Repeat password"
-              name="repeat-password"
+              name="password2"
               type="password"
-              autoComplete="current-password"
               className="textField"
               margin="normal"
             />
