b236b87c4d1826b0a1a705aa3bf1d0b67fcc83d3
Micro <microa4@gmail.com>
2019-04-27 17:51:58 +0200

    creando estilos

--- frontend/src/containers/Admin/bootcamps.jsx
+++ frontend/src/containers/Admin/bootcamps.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from '@reach/router';
+import './Bootcamps.scss';
 
 class AdminBootcamp extends Component {
 
