8a821b70ca52800738d05789050293a9dfc75e9a
Antoio <tono.ineba@gmail.com>
2019-04-30 09:53:58 +0200

    navigation actualizado

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -5,13 +5,51 @@ import { Link } from '@reach/router';
 
 import './Navigation.scss'
 
-const Navigation = (props) => (
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
 
-  <nav className="navigation">
 
-    <div className="logo">
-      logo
+
+
+      const Navigation = (props) => {
+
+        const styles = {
+          customWidth: {
+          width: 200,
+    }   
+  }
+    return(
+      <nav className="navigation">
+
+     <div className="logo">
+     logo
+
+      <div>
+
+        <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+          <MenuItem value={1} primaryText="Never" />
+          <MenuItem value={2} primaryText="Every Night" />
+          <MenuItem value={3} primaryText="Weeknights" />
+          <MenuItem value={4} primaryText="Weekends" />
+          <MenuItem value={5} primaryText="Weekly" />
+        </DropDownMenu>
+        <br />
+        <DropDownMenu
+          value={this.state.value}
+          onChange={this.handleChange}
+          style={styles.customWidth}
+          autoWidth={false}
+        >
+          <MenuItem value={1} primaryText="Custom width" />
+          <MenuItem value={2} primaryText="Every Night" />
+          <MenuItem value={3} primaryText="Weeknights" />
+          <MenuItem value={4} primaryText="Weekends" />
+          <MenuItem value={5} primaryText="Weekly" />
+        </DropDownMenu>
+  
       </div>
+      </div>
+  
     <div className="user">
 
       {
@@ -32,9 +70,13 @@ const Navigation = (props) => (
 
 
     </div>
-
+    
   </nav>
-);
-
+    )
+     
+    
+    }
+  
+    
 export default connect(state => ({ user: state.user }))(Navigation);
 
