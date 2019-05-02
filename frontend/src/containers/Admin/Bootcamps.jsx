8df7bb9ec46c33534c9a4250560d3b18b6042408
jupegarnica <jupegarnica@gmail.com>
2019-05-02 10:27:02 +0200

    admin/bootcamps refactor

--- frontend/src/containers/Admin/Bootcamps.jsx
+++ frontend/src/containers/Admin/Bootcamps.jsx
@@ -17,50 +17,8 @@ const AdminBootcamps = ({ bootcamps }) => (
         </Link>
       </div>
     ))}
-    {bootcamps.map(bootcamp => (
-      <div className='bootcamp'>
-        <h3>{bootcamp.title}</h3>
-        <Link edit='edit' to={'/admin/bootcamp/' + bootcamp._id}>
-          editar
-        </Link>
-        <Link edit='edit' to={'/bootcamp/' + bootcamp._id}>
-          ver
-        </Link>
-      </div>
-    ))}
   </div>
 );
-//     e = () => {
-//         const boot = [];
-//         // let caca = this.props.bootcamps;
-//         for (let index = 0; index < this.props.bootcamps.length; index++) {
-//             let ruta = '/admin/bootcamps/' + this.props.bootcamps[index]._id;
-//             boot.push(<div className=""><h3 className="">{this.props.bootcamps[index].title}</h3>
-//                 <Link edit='edit' to={ruta}> <strong>Edit</strong> </Link>
-//                 <Link edit='see' to={ruta}>  <strong>See</strong></Link>
-//             </div>)
-
-//         }
-//         return boot;
-//     }
-
-//     render() {
-
-//         //console.log(this.e())
-//         return (
-//             !this.e() ?
-//                 'No hay datos'
-//                 :
-//                 <div className="">
-//                     {this.e().map(function (x) {
-//                         return x;
-//                     })}
-//                     <button>+</button>
-//                 </div>
-//         );
-//     }
-
-// }
 
 const mapStateToProps = ({ bootcamps }) => ({ bootcamps });
 
