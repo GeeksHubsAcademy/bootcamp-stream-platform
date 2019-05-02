ca92ed82884a3c2ecc902e699d450e9f64d92302
DavidPinilla1 <metroid_300@hotmail.com>
2019-05-02 10:31:02 +0200

    response protocol defined thanks to Juan

--- backend/routes/bootcamp.js
+++ backend/routes/bootcamp.js
@@ -1,42 +1,43 @@
 const router = require('express').Router();
 const Bootcamp = require('../models/Bootcamp');
 const { authorization, isAdmin } = require('../utils/middleware/authorization');
-router.get('/mine', authorization, (req, res) => {
-  if (req.user.role === 'admin') return Bootcamp.find({})
-    .then(bootcamps => res.send(bootcamps))
-      .catch(res.send);
 
-  Bootcamp.find({ userIds:req.user._id})
-    .then(bootcamps => res.status(200).send(bootcamps))
-    .catch(res.send);
-});
-router.post('/new',authorization, isAdmin, (req, res) => {
-  new Bootcamp({
-    title: req.body.title,
-    description: req.body.description,
-    startsAt: req.body.startsAt,
-    weeksDuration: req.body.weeksDuration,
-    userIds: req.body.users,
-    postIds: [],
-  }).save().then(Bootcamp =>res.send(Bootcamp))
-  .catch(err=>res.status(500).json({err,message:"Something went wrong, our apologies"}))
-});
-router.patch('/update/:id',authorization, isAdmin, (req, res) => {
-  Bootcamp.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(bootcamp =>{
+const findAndResponseBootcamps=async(req, res) => {
+  try{
+    if (req.user.role === 'admin') {
+    const bootcamps = await Bootcamp.find({})
+    return res.status(200).send(bootcamps)
+  }
+   const bootcamps = await Bootcamp.find({ userIds:req.user._id})
+        res.status(200).send(bootcamps)
+  }catch(err){
+    res.json({err,message:"Something went wrong, our apologies"})
+  }
+};
+
+router.get('/mine', authorization, findAndResponseBootcamps);
+
+router.post('/new',authorization, isAdmin, async(req, res, next) => {
+  await new Bootcamp(req.body).save();
+  next();
+},findAndResponseBootcamps);
+
+router.patch('/:id',authorization, isAdmin, async (req, res, next) => {
+  await Bootcamp.findByIdAndUpdate(req.params.id, req.body);
+  next();
+},findAndResponseBootcamps );
+
+router.patch('/unsubscribed/:id',authorization, (req, res, next) => {
+  Bootcamp.findByIdAndUpdate(req.params.id,{$pull: { users:req.user._id}}).then(bootcamp =>{
     if(!bootcamp)return res.send('bootcamp not found')
-    res.send({bootcamp, message: "The bootcamp has been sucessfully updated"})})
-  .catch(err=>res.status(500).json({err,message:"Something went wrong, our apologies"}))
-});
-router.patch('/unsuscribe/:id',authorization, (req, res) => {
-  Bootcamp.findByIdAndUpdate(req.params.id, {users:users.filter(user=>user._id!==req.user._id)}, { new: true }).then(bootcamp =>{
-    if(!bootcamp)return res.send('bootcamp not found')
-    res.send({bootcamp, message: "You have been sucessfully unsuscribed"})})
-  .catch(err=>res.status(500).json({err,message:"Something went wrong, our apologies"}))
-});
-router.delete('/delete/:id',authorization,isAdmin, (req, res) => {
+    next();
+  })
+},findAndResponseBootcamps);
+
+router.delete('/delete/:id',authorization,isAdmin, (req, res,next) => {
   Bootcamp.findByIdAndDelete(req.params.id).then(bootcampDeleted =>{
     if(!bootcampDeleted)return res.send('bootcamp not found')
-    res.send(bootcampDeleted)})
-  .catch(err=>res.status(500).json({err,message:"Something went wrong, our apologies"}))
-});
+    next();
+  });
+},findAndResponseBootcamps);
 module.exports = router;
