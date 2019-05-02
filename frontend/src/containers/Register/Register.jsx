03d81fc26236d60cb5cf4b6675639aa314f5a8e7
Maksym paranchych <max.par.yur@live.com>
2019-05-02 15:59:28 +0200

    register recovered from juanma's

--- frontend/src/containers/Register/Register.jsx
+++ frontend/src/containers/Register/Register.jsx
@@ -1,110 +1,81 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PublicZone from '../../components/PublicZone';
-import { Link } from '@reach/router';
-
+import validator from 'validator';
+import { postRegister } from '../../redux/actions';
 import './Register.scss';
-
-
-
 
 class _Register extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
-      user: {
-        name: '',
-        lastname: '',
-        email: '',
-        password: '',
-        imagePath: '',
-        tokens: ''
-
-      },
-
-    }
-
-  }
-
-  handleChange(event) {
-    const { name, value } = event.target;
-    const { user } = this.state;
-    this.setState({
-      user: {
-        ...user,
-        [name]: value
-      }
-    });
-  }
-  Registering() {
-    if (this.props.state.user !== true) {
-      this.setState({ errorRegistr: 'Unvalid Data' })
+      name: '',
+      surname: '',
+      email: '',
+      password: '',
+      password2: '',
+      registrarme: '',
+      errorname: undefined,
+      errorsurname: undefined,
+      erroremail: undefined,
+      errorpassword: undefined,
+      errorparssword2: undefined,
     }
   }
-  //RegisterProces = event => {
-  //  event.preventDefault();
-  // let { password, email } = this.state;
 
-  //  if (password === '') {
-  //   this.setState({ errorPass: 'No has introducido el password' });
-  //  }
-  //  if (email === '') {
-  //   this.setState({ errorEmail: 'No has introducido el email' });
-  //  }
-  //  if (email !== email1) {
-  //   this.setState({ errorEmail: 'No has introducido el mismo email' });
-  // }
-  //  if (password !== email1) {
-  //   this.setState({ errorPass: 'No has introducido la misma contraseña' });
-  // }
-  // if (email === email1) {
-  //   this.setState({ errorEmail: 'No has introducido el mismo email' });
-  // } else {
-  //   console.log(password, email);
-  //   Registering(password, email)
-  //      .then(() => this.setState({ error: 'logged!!' }))
-  //     .catch(e => this.setState({ error: 'email o contraseña incorrecta' }));
-  // }
-  //};
+  handleChange = (ev) => {
+    console.log('ENTRA en handlechange');
+    this.setState({ [ev.target.name]: ev.target.value });
+    this.validate();
+  }
+  handleSubmit = (ev) => {
+    ev.preventDefault();//esto es para que no se refresque
+    console.log(this.state);
+    postRegister(this.name,this.surname,this.email,this.password,this.password2);
+    // loggedIn(pass, email)
+    //     .then(() => this.setState({ error: 'logged!!' }))
+    //     .catch(e => this.setState({ error: 'email o contraseÃ±a incorrecta' }));
+
+  }
+
+  validate() {
+    if (validator.isEmail(this.state.email)) {
+      this.setState({ erroremail: undefined });
+    }
+    else {
+      this.setState({ erroremail: 'Por favor, introduce un email vÃ¡lido' });
+    }
+  }
+
 
   render() {
 
-    const { user, submitted } = this.state;
+    const { errorname, errorsurname, erroremail, errorpassword, errorparssword2, name, surname, email, password, password2 }
+      = this.state;//esto lo hacemos para no tener que escribir tol rato this.name...
     return (
       <section className="RegisterView">
         <h1>Register</h1>
-        
-        <form onSubmit={this.login} className='loginView'>
-          <input name='name' placeholder='name' type='name' onChange={this.handleChange} />
-          {this.state.errorEmail && <div className='errorLoginView'>{this.state.errorEmail}</div>}
+        <form className="Form" onSubmit={this.handleSubmit}>
+          <input name="name" type="text" placeholder="name" onKeyUp={this.validate} value={name} />
+          <div className="error">{errorname}</div>
+          <input name="surname" type="text" placeholder="surname" onChange={this.handleChange} value={surname} />
+          <div className="error">{errorsurname}</div>
+          <input name="email" type="email" placeholder="email" onChange={this.handleChange} value={email} />
+          <div className="error">{erroremail}</div>
+          <input name="password" type="password" placeholder="password" onChange={this.handleChange} value={password} />
+          <div className="error">{errorpassword}</div>
+          <input name="password2" type="password" placeholder="repeat password" onChange={this.handleChange} value={password2} />
+          <div className="error">{errorparssword2}</div>
+          <input name="registrarme" type="submit" />
 
-          <input name='lastName' placeholder='lastName' type='lastName' onChange={this.handleChange} />
-          
-
-          <input name='email' placeholder='email' type='email' onChange={this.handleChange} />
-          {this.state.errorEmail && <div className='errorLoginView'>{this.state.errorEmail}</div>}
-
-          <input name='email' placeholder='email' type='email' onChange={this.handleChange} />
-          {this.state.errorEmail && <div className='errorLoginView'>{this.state.errorEmail}</div>}
-
-          <input name='password' placeholder='password' type='password' onChange={this.handleChange} />
-          {this.state.errorPass && <div className='errorLoginView'>{this.state.errorPass}</div>}
-
-          <input name='password' placeholder='password' type='password' onChange={this.handleChange} />
-          {this.state.errorPass && <div className='errorLoginView'>{this.state.errorPass}</div>}
-          
-          <div className="form-group">
-          <button>Login</button>
-            <Link to="/login" className="btn btn-link">Cancel</Link>
-          </div>
         </form>
       </section>
-    )
+
+    );
+
   }
 }
-
 
 const mapStateToProps = ({ user }) => ({ user });
 const mapDispatchToProps = dispatch => ({ dispatch });
