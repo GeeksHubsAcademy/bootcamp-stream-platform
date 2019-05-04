7251f4b90fd2058bc1d58c9e1881e324de2bbf1c
janfajessen <janfajessen@gmail.com>
2019-05-04 22:59:21 +0200

    bootcamp modified for not error

--- frontend/src/containers/Bootcamp/Bootcamp.jsx
+++ frontend/src/containers/Bootcamp/Bootcamp.jsx
@@ -6,14 +6,49 @@ import CreatePost from '../../components/CreatePost/CreatePost'
 const SearchPosts = () => <div>Search posts</div>
 
 // material-ui
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
+// import Card from '@material-ui/core/Card';
+// import CardHeader from '@material-ui/core/CardHeader';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardActions from '@material-ui/core/CardActions';
+// import Collapse from '@material-ui/core/Collapse';
+// import Avatar from '@material-ui/core/Avatar';
+// import IconButton from '@material-ui/core/IconButton';
+
+// const useStyles = makeStyles(theme => ({
+//   card: {
+//     maxWidth: 400,
+//   },
+//   media: {
+//     height: 0,
+//     paddingTop: '56.25%', // 16:9
+//   },
+//   actions: {
+//     display: 'flex',
+//   },
+//   expand: {
+//     transform: 'rotate(0deg)',
+//     marginLeft: 'auto',
+//     transition: theme.transitions.create('transform', {
+//       duration: theme.transitions.duration.shortest,
+//     }),
+//   },
+//   expandOpen: {
+//     transform: 'rotate(180deg)',
+//   },
+//   avatar: {
+//     backgroundColor: red[500],
+//   },
+// }));
+
+// function RecipeReviewCard() {
+//   const classes = useStyles();
+//   const [expanded, setExpanded] = React.useState(false);
+
+//   function handleExpandClick() {
+//     setExpanded(!expanded);
+//   }
+
 
 
 const Bootcamp = ({ bootcamp }) => {
@@ -32,11 +67,11 @@ const Bootcamp = ({ bootcamp }) => {
         ))}
       </div>
       <CreatePost />
-      {/* //////////////// */}
-      <Card className={classes.card}>
+
+      {/* <Card className='card'>
         <CardHeader
           avatar={
-            <Avatar aria-label='Recipe' className={classes.avatar}>
+            <Avatar aria-label='Recipe' className='avatar'>
               R
             </Avatar>
           }
@@ -54,7 +89,7 @@ const Bootcamp = ({ bootcamp }) => {
             This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
           </Typography>
         </CardContent>
-        <CardActions className={classes.actions} disableActionSpacing>
+        <CardActions className='actions' disableActionSpacing>
           <IconButton aria-label='Add to favorites'>
             <FavoriteIcon />
           </IconButton>
@@ -77,20 +112,16 @@ const Bootcamp = ({ bootcamp }) => {
             <Typography paragraph>Method:</Typography>
             <Typography paragraph>Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10 minutes.</Typography>
             <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly browned, 6 to 8
-              minutes. Transfer shrimp to a large plate and set aside, leaving chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook, stirring
-              often until thickened and fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+              Heat oil in a (14- to 16-inch) paella pan or a large....
             </Typography>
             <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-              medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook again without stirring, until mussels have opened and rice is just tender, 5 to 7 minutes more.
-              (Discard any mussels that don’t open.)
+              Add rice and stir very gently to distribute...
             </Typography>
             <Typography>Set aside off of the heat to let rest for 10 minutes, and then serve.</Typography>
           </CardContent>
         </Collapse>
-      </Card>
-      {/* //////////////// */}
+      </Card> */}
+
       <SearchPosts />
     </section>
   );
