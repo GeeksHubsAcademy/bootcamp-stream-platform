7499891fc6d6300cda0314f13e98482c9c26dfc3
jupegarnica <jupegarnica@gmail.com>
2019-05-06 16:49:33 +0200

    my bootcamps refactored

--- frontend/src/containers/Profile/MyBootcamps.jsx
+++ frontend/src/containers/Profile/MyBootcamps.jsx
@@ -4,25 +4,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from '@reach/router';
 
-import { getBootcamps} from '../../redux/actions'
 
 import './MyBootcamps.scss'
 
 class MyBootcamps extends Component {
 
-  componentDidMount() {
-    getBootcamps()
-  }
-
   render() {
     //console.log(JSON.stringify(this.props.bootcamps));
     console.log(this.props.bootcamps);
+    if (!this.props.bootcamps) {
+      return <div className="MyBootcamps">No bootcamps yet</div>
+    }
     return (
       <section className='MyBootcampsView'>
         <h3>Pertenece a los siguientes Bootcamps:</h3>
 
         <div className='MyBootcamps'>
-          {this.props.bootcamps.map(bootcamp => (
+          { this.props.bootcamps.map(bootcamp => (
             <Link to={'/bootcamp/' + bootcamp._id} className='listBootcamps' key={bootcamp._id}>
               {bootcamp.title}
             </Link>
