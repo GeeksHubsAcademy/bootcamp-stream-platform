dcc45d8dc622d784d880591733586bdee25fb39c
jupegarnica <jupegarnica@gmail.com>
2019-05-07 19:08:27 +0200

    create post in markdown new editor

--- frontend/src/components/CreatePost/CreatePostText.jsx
+++ frontend/src/components/CreatePost/CreatePostText.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import 'react-mde/lib/styles/css/react-mde-all.css';
-import ReactMde, { commands } from 'react-mde';
-const listCommands = [
-  {
-    commands: [commands.orderedListCommand, commands.unorderedListCommand, commands.checkedListCommand],
-  },
-];
-const CreatePostText = props => <ReactMde {...props}  commands={listCommands} />;
+import './CreatePostText.scss';
+import Editor from 'for-editor';
+
+const CreatePostText = props => <Editor {...props} placeholder="Escribe markdown..." />;
 
 export default CreatePostText;
