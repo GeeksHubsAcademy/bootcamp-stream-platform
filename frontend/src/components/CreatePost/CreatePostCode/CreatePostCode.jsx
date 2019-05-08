dbac8008e5e894f388572dfde14f15fa329f6cc5
jupegarnica <jupegarnica@gmail.com>
2019-05-08 17:47:43 +0200

    ace editor fully working

--- frontend/src/components/CreatePost/CreatePostCode/CreatePostCode.jsx
+++ frontend/src/components/CreatePost/CreatePostCode/CreatePostCode.jsx
@@ -4,16 +4,30 @@ import AceEditor from 'react-ace';
 
 import 'brace/mode/java';
 import 'brace/theme/github';
+import 'brace/theme/monokai';
 
 
 
-const Code = ({...props}) => (
+const Code = ({ ...props }) => (
     <AceEditor
     mode='javascript'
-    theme='github'
+    theme='monokai'
+    focus
+    fontSize={18}
+    width='100%'
+    height='100%'
+    setOptions={{
+        enableEmmet:true,
+        printMargin:true,
+        scrollPastEnd:true,
+        showInvisibles:false,
+         showPrintMargin:false,
+        showLineNumbers:true,
+         enableBasicAutocompletion:true,
+        enableLiveAutocompletion:true
+    }}
     editorProps={{ $blockScrolling: true }}
-    {...props}
-     />
+    {...props} />
 );
 
 export default Code;
