37072c0eb9cc64ebda741f814dff44128dc621f4
jupegarnica <jupegarnica@gmail.com>
2019-05-10 19:28:05 +0200

    permalink improved

--- frontend/src/components/DisplayPost/Post.jsx
+++ frontend/src/components/DisplayPost/Post.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PostText from './PostText';
 import PostActivity from './PostActivity';
@@ -10,9 +10,13 @@ import PostVideo from './PostVideo';
 import './Post.scss';
 
 const Post = ({ data, user }) => {
-  const [expanded, setExpanded] = useState(false)
+  const [expanded, setExpanded] = useState(false);
   function deletePost() {
     removePost(data._id);
+  }
+  function copyToClipboard() {
+   // TODO
+   // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard
   }
   let post;
   switch (data.postType) {
@@ -33,14 +37,14 @@ const Post = ({ data, user }) => {
       post = 'no valid postType';
   }
   return (
-    <div className={'postItem ' + (expanded ? 'expanded':'')} onClick={()=>setExpanded(!expanded)} id={data._id}>
-      <div className='actions'>
-        <a href={'#' + data._id}>
-          <FontAwesome icon='link' family='fas' />
+    <div className={'postItem ' + (expanded ? 'expanded' : '')} onClick={() => setExpanded(!expanded)} id={data._id}>
+      <div className='actions'>{user._id === data.authorId && <FontAwesome onClick={deletePost} icon='trash' family='fas' />}</div>
+
+      <h1>
+        <a href={'./#' + data._id} className='title' onClick={copyToClipboard}>
+          {data.content.title} <FontAwesome className='permalink' icon='link' family='fas' />
         </a>
-        {user._id === data.authorId && <FontAwesome onClick={deletePost} icon='trash' family='fas' />}
-      </div>
-      <h1 className='title'>{data.content.title}</h1>
+      </h1>
 
       {post}
     </div>
