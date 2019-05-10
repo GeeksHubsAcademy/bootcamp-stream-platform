fd776792ea1f1336b8c58661d4ae59ef5fee3a7b
jupegarnica <jupegarnica@gmail.com>
2019-05-10 16:45:33 +0200

    Create post video improved

--- frontend/src/components/CreatePost/CreatePostVideo.jsx
+++ frontend/src/components/CreatePost/CreatePostVideo.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import './CreatePostVideo.scss';
 import Reproductor from '../VideoPlayer';
 import { TextField } from '@material-ui/core';
+import FontAwesome from '../FontAwesome';
 
 const CreatePostVideo = ({ value, onChange }) => (
-    <div className='CreatePostVideo'>
-        <TextField
-            required id='body'
-            label='Url'
-            margin='normal'
-            onChange={ev => onChange(ev.target.value)}
-            value={value} />
-        {value && <Reproductor url={value} playing />}
-    </div>
+  <div className='CreatePostVideo'>
+    <TextField required id='body' label='Url' margin='normal' onChange={ev => onChange(ev.target.value)} value={value} />
+    {value ? (
+      <Reproductor url={value} playing />
+    ) : (
+      <div className='noPreview'>
+        <FontAwesome icon='video' />
+      </div>
+    )}
+  </div>
 );
 export default CreatePostVideo;
