c366cfea12409342e37ef5f4f425eabc277e85da
jupegarnica <jupegarnica@gmail.com>
2019-06-06 17:02:38 +0200

    conection to atlas on production

--- backend/config/environment.js
+++ backend/config/environment.js
@@ -6,33 +6,31 @@ const environments = {
 const ENV = process.env.NODE_ENV || environments.development;
 console.log( 'Your environment is ' + ENV )
 const config = {
-    [ environments.production ]: {
-        PORT: 80,
-        MongoDB: {
-            PORT: 27017,
-            HOST: 'localhost',
-            DB: 'Bootcamp-Stream-Platform'
-        }
+  [environments.production]: {
+    PORT: 80,
+    MongoDB: {
+      URI: 'mongodb+srv://dbUser:' + process.env.MONGOPASS + '@cluster0-bc0aq.gcp.mongodb.net/stream?retryWrites=true&w=majority'
     },
-    [ environments.development ]: {
-        PORT: 3001,
-        DOMAIN: 'localhost',
-        MongoDB: {
-            PORT: 27017,
-            HOST: 'localhost',
-            DB: 'Bootcamp-Stream-Platform_dev'
-        }
+  },
+  [environments.development]: {
+    PORT: 3001,
+    DOMAIN: 'localhost',
+    MongoDB: {
+      PORT: 27017,
+      HOST: 'localhost',
+      DB: 'Bootcamp-Stream-Platform_dev',
     },
+  },
 
-    [ environments.test ]: {
-        PORT: 3001,
-        MongoDB: {
-            PORT: 27017,
-            HOST: 'localhost',
-            DB: 'Bootcamp-Stream-Platform_test'
-        }
-    }
-}
+  [environments.test]: {
+    PORT: 3001,
+    MongoDB: {
+      PORT: 27017,
+      HOST: 'localhost',
+      DB: 'Bootcamp-Stream-Platform_test',
+    },
+  },
+};
 const CONFIG = config[ ENV ]
 if ( !CONFIG ) {
     throw new Error( `NODE_ENV ${ENV} is not a valid environment. ` )
--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require( 'mongoose' );
-const { PORT, DB, HOST } = process.env.MongoDB;
-mongoose.connect( `mongodb://${HOST}:${PORT}/${DB}`, { useCreateIndex: true, useNewUrlParser: true } )
+const { PORT, DB, HOST, URI } = process.env.MongoDB;
+mongoose.connect(URI || `mongodb://${HOST}:${PORT}/${DB}`, { useCreateIndex: true, useNewUrlParser: true } )
     .then( () => console.log( `Connected to database mongodb://${HOST}:${PORT}/${DB}` ) )
     .catch( ( e ) => console.log( 'Connection to MongoDB failed!:( \n' + e ) )
 
