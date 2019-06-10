09320bddb9b61cdfd763e571b049d00db4a18943
jupegarnica <jupegarnica@gmail.com>
2019-06-10 16:52:27 +0200

    heartbeat backend logic working

--- backend/models/Presence.js
+++ backend/models/Presence.js
@@ -16,9 +16,14 @@ const PresenceSchema = new mongoose.Schema(
         type: Number,
         required: true,
       },
+      accuracy: {
+        type: Number,
+        required: true,
+      },
     },
     registeredMilliseconds: {
       type: Number,
+      default: 0,
       //   required: true,
     },
   },
@@ -27,6 +32,52 @@ const PresenceSchema = new mongoose.Schema(
   },
 );
 
-const PresenceModel = mongoose.model('presence', PresenceSchema);
+const DELTA_DISTANCE = 1; // TODO FIX THIS VALUE
+const DELTA_TIME = 1000 * 60 * 16  ; // 16MIN
+
+PresenceSchema.methods.isNear = function(coords, accuracyGap = DELTA_DISTANCE) {
+  const from = this.location;
+  const delta = {
+    latitude: from.latitude - coords.latitude,
+    longitude: from.longitude - coords.longitude,
+  };
+  const absModule = Math.sqrt(delta.latitude ** 2 + delta.longitude ** 2);
+  console.log('isNear absModule:', absModule);
+
+  return absModule <= accuracyGap;
+};
+
+PresenceSchema.methods.elapsedTime = function() {
+  const from = new Date(this.updatedAt);
+  const to = new Date();
+  const delta = to.getTime() - from.getTime();
+  console.log('elapsedTime delta:', delta);
+
+  return delta;
+};
+
+PresenceSchema.statics.findLast = function(userId) {
+  return this.findOne({ userId }, {}, { sort: { updatedAt: -1 } }).exec();
+};
+
+PresenceSchema.statics.newRecord = async function(location, userId) {
+  let lastRecord = await this.findLast(userId);
+
+  if (
+    lastRecord &&
+    lastRecord.elapsedTime() <= DELTA_TIME &&
+    lastRecord.isNear(location)
+  ) {
+    console.log('updated precense document ' + lastRecord._id);
+    const elapsedTime = lastRecord.elapsedTime();
+    lastRecord.registeredMilliseconds += elapsedTime;
+    return lastRecord.save();
+  } else {
+    console.log('new precense document ');
+    return new PresenceModel({ location, userId }).save();
+  }
+};
+
+var PresenceModel = mongoose.model('presence', PresenceSchema);
 
 module.exports = PresenceModel;
--- backend/routes/heartbeat.js
+++ backend/routes/heartbeat.js
@@ -5,25 +5,20 @@ const Presence = require('../models/Presence');
 const { authorization } = require('../middleware/authorization');
 
 router.post('/', authorization, async (req, res) => {
-  console.log('new heartbeat', req.body, req.user._id);
-  res.send(req.body);
 
-  const { latitude, longitude } = req.body;
+
+  const { latitude, longitude , accuracy} = req.body;
   const userId = req.user._id;
   try {
-    let res = await new Presence({
-      location: {
-        latitude,
-        longitude,
-      },
-      userId,
-    }).save();
-    console.log(res);
+    let record = await Presence.newRecord({ latitude, longitude, accuracy }, userId);
+     res.json({
+       message:
+         'elapsed time since last heartbeat: ' +
+         (record.registeredMilliseconds / (60 * 1000)).toFixed(1) + ' minutes',
+     });
   } catch (error) {
+     res.status(500).json({ message: 'ups' });
     console.error(error);
   }
-
-  // Presence.find( {} ).then( users => res.send( users ) )
-  //     .catch( error => res.status( 500 ).send( error ) );
 });
 module.exports = router;
